2fe1773a8193219c35b619d407b22f41e2179fdf
ntr18 <nghia.nguyen.tr18@gmail.com>
2023-11-16 01:27:11 +0700

    update database variable

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,8 +2,8 @@ const { Sequelize } = require('sequelize');
 
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('2handwarehouse', 'root', null, {
-    host: 'localhost',
+const sequelize = new Sequelize('bouqa5foi1fh2meaxnzq', 'uylrtcg8gtr3lk0l', 'ZC6HGeaQU3jpmL9BsBbb', {
+    host: 'bouqa5foi1fh2meaxnzq-mysql.services.clever-cloud.com',
     dialect: 'mysql',
     // loggin don't log execute in terminal
     logging: false
