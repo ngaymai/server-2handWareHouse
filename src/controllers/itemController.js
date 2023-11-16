013eda4c8c1c27a363f5afeccf00b2c61559089d
Long Le <lhlong1542002@gmail.com>
2023-11-16 16:20:57 +0700

    add item query

---
+++ src/controllers/itemController.js
@@ -0,0 +1,24 @@
+
+let handleGetItems = async (req, res) => {
+    let id = req.query.id; // all - for list of all iteams, id - for 1 item id
+
+    if (!id) {
+        return res.status(500).json({
+            errCode: 1,
+            errMessage: 'Missing parameter value',
+            users: []
+        })
+    }
+
+    let items = await itemService.getItems(id);
+
+    return res.status(200).json({
+        errCode: 0,
+        errMessage: 'OK',
+        items
+    })
+}
+
+module.exports = {
+    handleGetItems: handleGetItems
+}
