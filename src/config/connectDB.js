1328b24fc18c1c157a77884d7970c92d3d25551c
ntr18 <nghia.nguyen.tr18@gmail.com>
2023-11-16 13:06:13 +0700

    change to postgres

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,14 +1,31 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 
-// Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('bouqa5foi1fh2meaxnzq', 'uylrtcg8gtr3lk0l', 'ZC6HGeaQU3jpmL9BsBbb', {
-    host: 'bouqa5foi1fh2meaxnzq-mysql.services.clever-cloud.com',
-    dialect: 'mysql',
-    // loggin don't log execute in terminal
-    logging: false
-    /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
-});
+// Option 2: Passing parameters separately (other dialects)
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        logging: false,
+        dialectOptions:
+            process.env.DB_SSL === 'true' ?
+                {
+                    ssl: {
+                        require: true,
+                        rejectUnauthorized: false
+                    }
+                } : {}
+        ,
+        query: {
+            "raw": true
+        },
+        timezone: "+07:00"
+    });
 
 let connectDB = async () => {
     try {
@@ -18,5 +35,4 @@ let connectDB = async () => {
         console.error('Unable to connect to the database:', error);
     }
 }
-
 module.exports = connectDB;
